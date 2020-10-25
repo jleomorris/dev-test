a196d02fec0f670ab299cd17451ef2f9b371cde2
James Morris <70540466+jleomorris@users.noreply.github.com>
2020-10-25 01:41:08 +0000

    Remove props from Accordion component

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -8,61 +8,7 @@ export default class App extends Component {
     return (
       <>
         <div className="accordion__container">
-          <Accordion
-            title="Step 1: Your details"
-            content="
-            <div class='form-container'>
-              <div class='detail'>
-                <p>First Name</p>
-                <input></input>
-              </div>
-              <div class='detail'>
-                <p>Surname</p>
-                <input></input>
-              </div>
-              <div class='detail'>
-                <p>Email address</p>
-                <input></input>
-              </div>
-            </div>
-            <button class='next__button'>Next ></button>"
-          />
-          <Accordion
-            title="Step 2: More comments"
-            content=" 
-            <div class='form-container'>
-              <div class='detail'>
-                <p>Telephone number</p>
-                <input></input>
-              </div>  
-              <div class='detail'>
-                <p>Gender</p>
-                <select>
-                  <option>Select Gender</option>
-                  <option>Male</option>
-                  <option>Female</option>
-                </select>
-              </div>  
-              <div class='detail'>
-                <p>Date of birth</p>
-                <input class='date__of__birth__input'></input>
-                <input class='date__of__birth__input'></input>
-                <input class='date__of__birth__input'></input>
-              </div>  
-            </div>
-            <button class='next__button'>Next ></button>"
-          />
-          <Accordion
-            title="Step 3: Final comments"
-            content=" 
-            <div class='form-container'>
-              <div class='detail'>
-                <p>Comments</p>
-                <input class='comments__input'></input>
-              </div>
-            </div>
-            <button class='next__button'>Next ></button>"
-          />
+          <Accordion />
         </div>
       </>
     );
